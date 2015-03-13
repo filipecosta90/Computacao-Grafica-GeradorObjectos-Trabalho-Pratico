1b41c3fd75ae44771d2c48523fca0366a3b14be6
Sérgio Caldas <sergiocaldas.91@gmail.com>
2015-03-13 19:16:54 +0000

    O gerador já gera o ficheiro com os pontos do plano e do paralelipipedo

--- Gerador.cpp
+++ Gerador.cpp
@@ -1,22 +1,98 @@
 #include <iostream>
 #include <fstream>
 
-void plano(float largura, float comprimento){
+void plano(float largura, float comprimento, char *file_name){
 
-	FILE *f;
-	f = fopen("plano.3d", "w");
+	FILE* filename;
 
-	fprintf(f, "%f %f %f\n", comprimento / 2, 0.0, largura / 2);
-	fprintf(f, "%f %f %f\n", comprimento / 2, 0.0, -largura / 2);
-	fprintf(f, "%f %f %f\n", -comprimento / 2, 0.0, largura / 2);
+	filename = fopen(file_name, "w");
 
-	fprintf(f, "%f %f %f\n", comprimento / 2, 0.0, largura / 2);
-	fprintf(f, "%f %f %f\n", -comprimento / 2, 0.0, -largura / 2);
-	fprintf(f, "%f %f %f\n", -comprimento / 2, 0.0, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, 0.0, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, 0.0, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, 0.0, largura / 2);
 
-	fclose(f);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, 0.0, largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, 0.0, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, 0.0, largura / 2);
+
+	fclose(filename);
 }
 
-int main(){
-	plano(10.0, 10.0);
+void paralelipipedo(float largura, float comprimento, float altura, char *file_name){
+	FILE *filename;
+
+	filename = fopen(file_name, "w");
+
+	//Base 1
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, largura / 2);
+
+	//Base 2 
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, -largura / 2);
+
+	//Lado 1.1
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, largura / 2);
+
+	//Lado 1.2
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, largura / 2);
+
+	//Topo 1
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, -largura / 2);
+
+	//Topo 2
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, -largura / 2);
+
+	//Lado 2.1
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, -largura / 2);
+
+	//Lado 2.2
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, -largura / 2);
+
+	//Frente 1
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, largura / 2);
+
+	//Frente 2
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, largura / 2);
+
+	//Tras 1
+	fprintf(filename, "%f %f %f\n", comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, -largura / 2);
+
+	//Tras 2
+	fprintf(filename, "%f %f %f\n", comprimento / 2, altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, -altura / 2, -largura / 2);
+	fprintf(filename, "%f %f %f\n", -comprimento / 2, altura / 2, -largura / 2);
+
+	fclose(filename);
+}
+
+int main(int argc, char **argv){
+	/*switch (argv[1]){
+		case "plano": plano(argv[2], argv[3], argv[4]);
+			break;
+		default:
+			break;
+	}*/
+	plano(10.0, 10.0,"plano.3d");
+	paralelipipedo(12,12,12,"paralelipipedo.3d");
 }
