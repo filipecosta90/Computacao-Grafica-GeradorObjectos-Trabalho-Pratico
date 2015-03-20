1705a83731f53c4324101df405287495f5a1cf3d
Filipe RdF Oliveira <filipecosta.90@gmail.com>
2015-03-20 15:15:19 +0000

    corrigi erros

--- Gerador.cpp
+++ Gerador.cpp
@@ -8,22 +8,53 @@
 #include "model.hpp"
 
 int main( int argc, char* argv[] ){
-  Model model(argv[1]);
-  if( strcmp ( argv[1] , "plano" ) == 0 ){
-	  model.planoXZ_Yfixo(atof(argv[2]), 0, atof(argv[3]), atof ( argv[4]), atof (argv[5] ), 1 );
-    model.save(argv[6]);
+  bool parametersOk = true;
+  if (argc > 1){
+	  Model model(argv[1]);
+	  if (strcmp(argv[1], "plano") == 0){
+		  if (argc == 7){
+			  //plano comprimento largura camadasComprimento camadasLargura nomeficheiro
+			  model.planoXZ_Yfixo(atof(argv[2]), 0, atof(argv[3]), atof(argv[4]), atof(argv[5]), 1);
+			  model.save(argv[6]);
+		  }
+		  else{ parametersOk = false; }
+	  }
+	  if (strcmp(argv[1], "paralelipipedo") == 0){
+		  //paralelipipedo comprimento altura largura camadasComprimento camadasAltura camadasLargura nomeficheiro
+		  if (argc == 9){
+			  model.createParallelepipe(atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]));
+			  model.save(argv[8]);
+		  }
+		  else { parametersOk = false; }
+	  }
+	  if (strcmp(argv[1], "cone") == 0){
+		  if (argc == 7){
+			  //cone raio altura fatias camadas nomeficheiro
+			  model.createCone(atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]));
+			  model.save(argv[6]);
+		  }
+		  else { parametersOk = false; }
+	  }
+	  if (strcmp(argv[1], "esfera") == 0){
+		  if (argc == 6){
+			  //esfera raio fatias camadas nomeficheiro
+			  model.createSphere(atof(argv[2]), atof(argv[3]), atof(argv[4]));
+			  model.save(argv[5]);
+		  }
+		  else { parametersOk = false; }
+	  }
+	  else {
+		  //nao reconhece o sólido pedido
+		  parametersOk = false;
+	  }
   }
-  if( strcmp ( argv[1] , "paralelipipedo" ) == 0  ){
-	  model.createParallelepipe(atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]) );
-    model.save(argv[8]);
+  else {
+	  parametersOk = false;
   }
-  if( strcmp ( argv[1] , "cone" ) == 0  ){
-    model.createCone ( atof ( argv[2] ) , atof ( argv[3] ) , atof ( argv[4] ) , atof ( argv[5] )  );
-    model.save(argv[6]);
+  if (parametersOk){
+	  return EXIT_SUCCESS;
   }
-  if( strcmp ( argv[1] , "esfera" ) == 0  ){
-    model.createSphere ( atof ( argv[2] ) , atof ( argv[3] ) , atof ( argv[4] )  );
-    model.save(argv[5]);
+  else{
+	  return EXIT_FAILURE;
   }
-  return 0;
 }
